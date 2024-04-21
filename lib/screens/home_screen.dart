48e0053df40cf75d8b1495d94b5423a2b4f2938e
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2024-04-21 22:42:01 +0530

    Created UI statically.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -61,7 +61,166 @@ class HomeScreen extends StatelessWidget {
               BackdropFilter(filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
               child: Container(
                 decoration: const BoxDecoration(color: Colors.transparent),
-              ),)
+              ),),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child:  Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('📌 Nittambuwa', style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500
+                    ),),
+                    const SizedBox(height: 8,),
+                    const Text('Good Morning', style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold
+                    ),),
+                    Image.asset('assets/4.png'),
+                    const Center(
+                      child: Text('21˚C',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 55,
+                        fontWeight: FontWeight.w600
+                      ),),
+                    ),
+                    const Center(
+                      child: Text('SNOW',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500
+                        ),),
+                    ),
+                    const SizedBox(height: 5,),
+                    const Center(
+                      child: Text('Friday 15 - 09.41am',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w300
+                        ),),
+                    ),
+                    const SizedBox(height: 90,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(''
+                                'assets/11.png',
+                            scale: 8,),
+                            const SizedBox(width: 5,),
+                          const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Sunrice',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w300
+                                ),),
+                              SizedBox(height: 3,),
+                              Text('05.34 am',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700
+                                ),),
+                            ],
+                          )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset(''
+                                'assets/12.png',
+                              scale: 8,),
+                            const SizedBox(width: 5,),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Sunset',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300
+                                  ),),
+                                SizedBox(height: 3,),
+                                Text('06.34 pm',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700
+                                  ),),
+                              ],
+                            )
+                          ],
+                        ),
+
+                      ],
+                    ),
+                    const Padding(padding: EdgeInsets.symmetric(vertical: 5.0),
+                    child: Divider(
+                      color: Colors.grey,
+                    ),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(''
+                                'assets/13.png',
+                              scale: 8,),
+                            const SizedBox(width: 5,),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Max Temp',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300
+                                  ),),
+                                SizedBox(height: 3,),
+                                Text('21˚C',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700
+                                  ),),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset(''
+                                'assets/14.png',
+                              scale: 8,),
+                            const SizedBox(width: 5,),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Min Temp',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w300
+                                  ),),
+                                SizedBox(height: 3,),
+                                Text('12˚C',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w700
+                                  ),),
+                              ],
+                            )
+                          ],
+                        ),
+
+                      ],
+                    ),
+
+                  ],
+                ),
+              )
             ],
           ),
         ),
